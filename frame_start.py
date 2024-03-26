395c8dc68b6c0e8e0168b1128f79e0abb9c3115b
justflying <jonglae@gmail.com>
2024-03-26 16:44:20 +0900

    Update frame_start.py
    최적화

--- frame_start.py
+++ frame_start.py
@@ -1,34 +1,30 @@
- #!/usr/bin/env python3
-from tkinter import *
+#!/usr/bin/env python3
+import tkinter as tk
 import subprocess
-import os
 
-root = Tk()
+def start_game():
+    user_input = text_box.get("1.0", tk.END).strip()
+    if user_input.startswith("daumgamestarter://"):
+        command = f"GDK_BACKEND=x11 nohup python3 ./fakeDaumgameStarter.py '{user_input}' >/dev/null 2>&1"
+        subprocess.Popen(command, shell=True)
+        root.destroy()
+    else:
+        status_label.config(text="잘못된 입력입니다. 올바른 입력을 해주세요.")
+
+root = tk.Tk()
 root.title("리눅스 POE 게임 스타터")
-root.geometry("800x200+500+1500")
+root.geometry("800x250+500+1500")
 
-def exit_windows():
-    root.destroy()
+text_box = tk.Text(root, width=160, height=10)
+text_box.pack()
 
-txt = Text(root, width=160, height=10)
-txt.pack()
-scr_txt1 = txt.pack()
-# print("srctxt",scr_txt1)
+start_button = tk.Button(root, text="게임시작", command=start_game)
+start_button.pack()
 
-def write_text():
-    scr_txt2= txt.get("1.0","end")
-    result = scr_txt2[24:]
-    result = "nohup python3 "+"./fakeDaumgameStarter.py "+"'daumgamestarter://"+result+"'"+">/dev/null 2>&1"
-    print("입력받은 텍스트",result)
-    # os.system('cd')
-    # os.system('pwd')
-    os.system(result)
-    exit_windows()
+exit_button = tk.Button(root, text="EXIT", command=root.destroy)
+exit_button.pack()
 
-btn = Button(root, text="게임시작",command=write_text)
-btn2 = Button(root, text="EXIT",command=exit_windows)
-
-btn.pack()
-btn2.pack()
+status_label = tk.Label(root, text="")
+status_label.pack()
 
 root.mainloop()
